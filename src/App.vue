4c994ef415d870f854b7931ae6e143157945802c
skipandgoon <72314550+skipandgoon@users.noreply.github.com>
2026-05-02 00:15:31 +0800

    更新 App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import { NGlobalStyle, NMessageProvider, NNotificationProvider, darkTheme } from
 import { darkThemeOverrides, lightThemeOverrides } from './themes';
 import { layouts } from './layouts';
 import { useStyleStore } from './stores/style.store';
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n()
+locale.value = 'zh'
 
 const route = useRoute();
 const layout = computed(() => route?.meta?.layout ?? layouts.base);
